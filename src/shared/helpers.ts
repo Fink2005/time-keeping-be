046d13bb912617332a55f09dfd03010b07f63143
Fink2005 <phans3806@gmail.com>
2025-09-19 16:21:28 +0700

    fix(change import prisma): change import Prisma
    change import Prisma in helpers

--- src/shared/helpers.ts
+++ src/shared/helpers.ts
@@ -1,9 +1,8 @@
 import { Prisma } from '@prisma/client';
-import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 // type Predicate
 export const isUniqueConstraintPrismaError = (
   error: any,
-): error is PrismaClientKnownRequestError => {
+): error is Prisma.PrismaClientKnownRequestError => {
   return (
     error instanceof Prisma.PrismaClientKnownRequestError &&
     error.code === 'P2002'
@@ -12,7 +11,7 @@ export const isUniqueConstraintPrismaError = (
 
 export const isNotFoundPrismaError = (
   error: any,
-): error is PrismaClientKnownRequestError => {
+): error is Prisma.PrismaClientKnownRequestError => {
   return (
     error instanceof Prisma.PrismaClientKnownRequestError &&
     error.code === 'P2025'
