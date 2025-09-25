30c478582e84881005c9fe6ea2d7bf1832d7db83
toiladat <tdat9663@gmail.com>
2025-09-25 10:32:56 +0700

    chore: show query UI swagger

--- src/shared/dtos/request.dto.ts
+++ src/shared/dtos/request.dto.ts
@@ -1,7 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { createZodDto } from 'nestjs-zod';
 import {
   EmptyBodySchema,
   PaginationQuerySchema,
 } from './../models/request.model';
 export class EmptyBodyDTO extends createZodDto(EmptyBodySchema) {}
-export class PaginationQueryDTO extends createZodDto(PaginationQuerySchema) {}
+export class PaginationQueryDTO extends createZodDto(PaginationQuerySchema) {
+  @ApiProperty({ default: 1 })
+  page: number;
+
+  @ApiProperty({ default: 5 })
+  limit: number;
+}
