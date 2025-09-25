d12075f81ff174d2b77b70548e625a32c76ac93a
toiladat <tdat9663@gmail.com>
2025-09-25 22:59:46 +0700

    feat: upload to cloudinary

--- src/shared/models/response.model.ts
+++ src/shared/models/response.model.ts
@@ -1,5 +1,9 @@
+import { UploadApiErrorResponse, UploadApiResponse } from 'cloudinary';
 import { z } from 'zod';
+
 export const MessageResSchema = z.object({
   message: z.string(),
 });
 export type MessageResType = z.infer<typeof MessageResSchema>;
+
+export type CloudinaryResponse = UploadApiResponse | UploadApiErrorResponse;
