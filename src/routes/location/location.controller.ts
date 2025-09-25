ed661bbf2c1cc1c89e53bb990bbaeeaa985a70b8
toiladat <tdat9663@gmail.com>
2025-09-25 18:20:36 +0700

    feat: delete location api & remove regex to validate lng,lat

--- src/routes/location/location.controller.ts
+++ src/routes/location/location.controller.ts
@@ -1,7 +1,16 @@
-import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { ApiBearerAuth, ApiResponse } from '@nestjs/swagger';
 import { ZodSerializerDto } from 'nestjs-zod';
 import { ActivateUser } from 'src/shared/decorators/activate-user.decorator';
+import { MessageResDTO } from 'src/shared/dtos/response.dto';
 import {
   CreateLocationBodyDTO,
   GetDetailLocationResDTO,
@@ -41,5 +50,17 @@ export class LocationController {
     @Body() body: UpdateLocationBodyDTO,
   ) {
     return this.locationService.updateLocation({ userId, body });
+  }
+
+  @Delete('/:id')
+  @ApiResponse({ status: 200, type: MessageResDTO })
+  deleteLocation(
+    @Param() params: GetLocationParamsDTO,
+    @ActivateUser('userId') userId: number,
+  ) {
+    return this.locationService.deleteLocation({
+      id: Number(params.id),
+      userId,
+    });
   }
 }
--- src/shared/models/shared-location.model.ts
+++ src/shared/models/shared-location.model.ts
@@ -1,11 +1,10 @@
 import z from 'zod';
-import { validAccordiate } from '../constants/attendance.constant';
 
 export const LocationSchema = z.object({
   id: z.number().int(),
   name: z.string().max(255),
-  lat: z.string().regex(validAccordiate.LAT, 'Tọa độ không hợp lệ'),
-  lng: z.string().regex(validAccordiate.LNG, 'Tọa độ không hợp lệ'),
+  lat: z.string(),
+  lng: z.string(),
   address: z.string().max(500),
   radius: z.number().default(50),
   createdAt: z.date(),
